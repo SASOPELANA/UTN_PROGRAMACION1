075f5c230778fa81343babcfaed4170462dc3dfc
sergio <sopekof@gmail.com>
2024-04-19 01:08:37 -0300

    19/04/24

---
+++ Unidad3_While/cpp/Ejercicio1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /**
+    1 Hacer un programa para mostrar por pantalla los números del 1 al 10.
+      Importante: El programa no tiene ningún ingreso de datos.
+    */
+
+    int num = 1;
+
+    while (num <= 10)
+    {   
+        cout << num << endl;
+        num++;
+    }
+    
+    return 0;
+}
