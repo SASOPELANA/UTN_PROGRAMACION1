6aab120e11cb887b9df8557ada64b80a254ef8d7
sergio <sopekof@gmail.com>
2024-04-24 13:11:01 -0300

    24/04/24

---
+++ Unidad3_While/cpp/Ejercicio19.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*
+    19 Se define como divisores propios de un número a aquellos que son sus divisores excluyendo al número en sí mismo.
+       Ejemplo A. Los divisores propios del 4 son: 1 y 2.
+       Ejemplo B. Los divisores propios del 12 son; 1, 2, 3, 4 y 6.
+       Se define a un número como perfecto cuando la suma de todos sus divisores propios coincide con el número en sí mismo.
+       Ejemplo A: 6 es número perfecto pues 1+2+3=6
+       Ejemplo B: 28 es número perfecto pues 1+2+4+7+14=28
+       Ejemplo: 12 no es número perfecto pues 1+2+3+4+6=16
+       Hacer un programa para ingresar un número y luego informar con un cartel aclaratorio si el mismo es un número perfecto o no es un número perfecto
+    */
+
+    int num, x = 1, acu_divisible = 0;
+
+    cout << "Diguite un número: " << endl;
+    cin >> num;
+
+    while (x < num)
+    {
+        if (num % x == 0){
+            acu_divisible += x;
+        }
+        x++;
+    }
+    
+    if (acu_divisible == num){
+        cout << "El número es perfecto." << endl;
+    }else{
+        cout << "No es un número perfecto." << endl;
+    }
+    
+    return 0;
+}
---
+++ Unidad3_While/cpp/Ejercicio20.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*
+    20 Dada una lista de números que finaliza cuando se ingresa un cero, informar el primer número par ingresado y su ubicación en la lista y el último de los
+       números primos y su ubicación en la lista.
+       Ejemplo A: 7, 4, 5, 6, 9, 13, 10 se informa Primer número par: 4 ubicación 2.
+       Último primo: 13 ubicación 6.
+       Ejemplo B: 9, 5, 21, 9, 13, 15, 6 se informa Primer número par: 6 ubicación 7.
+       Último primo: 13 ubicación 5.
+    */
+
+    int num, x = 1, i = 1, primer_par, posicion_par = 0, primo = 0,con_primos = 0, posicion_primos = 0;
+    bool bandera = true, primosss = false;
+
+    cout << "Diguite una lista de números o cero (0) para finalizar: " << endl;
+    cin >> num;
+
+    while (num != 0)
+    {
+        if (num % 2 == 0){
+            if (bandera){
+                primer_par = num;
+                posicion_par = x;
+                bandera = false;
+            } 
+        }
+        
+        con_primos = 0; 
+        i = 1;
+        while (i <= num)
+        {
+            if (num % i == 0){
+                con_primos++;
+            }
+            i++;
+        }
+        if (con_primos == 2){
+            primo = num;
+            posicion_primos = x;
+            primosss = true;
+        }
+        x++;
+
+        cout << "Diguite una lista de números o cero (0) para finalizar: " << endl;
+        cin >> num;
+    }
+    
+    if (!bandera){
+        cout << "El primer número par es: " << primer_par << " y su ubicación " << posicion_par << endl;
+    }else{
+        cout << "No se ingresaron números pares." << endl;
+    }
+
+    if (primosss){
+        cout << "El ultimo número primo es: " << primo << " y su ubicaión: " << posicion_primos << endl;
+    }else{
+        cout << "No se ingresaron numeros primos" << endl;
+    }
+
+    return 0;
+}
