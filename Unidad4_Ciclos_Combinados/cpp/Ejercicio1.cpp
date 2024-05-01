8b71970e5d02fa44bd068feb3edf642a44b22daf
sergio <sopekof@gmail.com>
2024-05-01 00:18:27 -0300

    01/05/24

--- Unidad4_Ciclos_Combinados/cpp/Ejercicio1.cpp
+++ Unidad4_Ciclos_Combinados/cpp/Ejercicio1.cpp
@@ -7,7 +7,26 @@ using namespace std;
 
 int main(){
 
+    int num, x = 0, i = 0, acu_divisible = 0, num_perfecto = 0;
+
+    for ( x = 0; x < 10; x++)
+    {
+        cout << "Diguite numeros: " << endl;
+        cin >> num;
+
+        acu_divisible = 0;
+        for (i = 1; i < num; i++)
+        {
+            if (num % i == 0){
+                acu_divisible += i;
+            }
+        }
+        if (acu_divisible == num){
+            num_perfecto++;
+        }  
+    }
     
+    cout << "Los numeros perfectos que se ingresaron son: " << num_perfecto << endl;
 
     return 0;
 }
