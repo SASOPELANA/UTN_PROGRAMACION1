a1a998ddfffaefb1e40f61631709a8a4bd5945cb
sergio <sopekof@gmail.com>
2024-05-14 11:18:17 -0300

    14/05/24

---
+++ Unidad4_Ciclos_Combinados/cpp/Ejercicio9.cpp
@@ -0,0 +1,21 @@
+/*
+    9 Hacer un programa para ingresar los valores de los pesos de distintas encomiendas que 
+    se deben enviar a distintos clientes y que finaliza cuando se ingresa un peso negativo. 
+    Se deben agrupar las encomiendas en camiones que pueden transportar hasta 200 kilos en total.
+    Ejemplo:  10, 20, 140,   70, 100,    40, 10, 50, 80,   90, 30, 40,     50, -10
+    Camión:       1              2              3               4           5
+    Se pide determinar e informar:
+    a) El número de cada camión y peso total de encomiendas. Camión 1: 170 kg, Camión 2: 170 kg, etc.
+    b) El número de camión que transporta mayor cantidad de encomiendas. 
+    En el ejemplo anterior sería el Camión 3 con 4 encomiendas.
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    
+
+
+    return 0;
+}
