f468eee3ac80a397c78aa8406858294edd8cc401
sergio <sopekof@gmail.com>
2024-05-14 10:14:52 -0300

    uu

--- Unidad4_Ciclos_Combinados/cpp/Ejercicio8.cpp
+++ Unidad4_Ciclos_Combinados/cpp/Ejercicio8.cpp
@@ -16,15 +16,22 @@ using namespace std;
 
 int main(){
 
-    int num, x = 0, anterior = 0, con = 0;
-    bool banadera_mayor = true;
-    int primos = 0, con_primos = 0, i = 0;
+    int num, x = 0, anterior = 0, con = 0; // Estructura
+    bool banadera_mayor = true; // Estructura
+    int primos = 0, con_primos = 0, i = 0; // A
+    int par = 0, par_menor = 0; // B
+    int posi = 0, ante_posi = 0, ultimo_posi = 0; // C
 
     for (x = 1; x <= 5; x++)
     {   
         // Punto A
         con_primos = 0;
         primos = 0;
+        // Punto B
+        par = 0;
+        par_menor = 0;
+        // Punto C
+        posi = 0;
 
         banadera_mayor = true;
         con = 0;
@@ -36,7 +43,8 @@ int main(){
             con++;
             
             // Punto A
-            if(num > 0){
+            if(num > 1){
+                con_primos = 0;
                 for (i = 1; i <= num; i++)
                 {
                     if(num % i == 0){
@@ -47,12 +55,30 @@ int main(){
                     primos++;
                 }  
             }
-            
-            
 
+            // Punto B
+            if(num % 2 == 0 && num != 0){
+                par++;
+                if(par == 1){
+                    par_menor = num;
+                }else if(num < par_menor){
+                    par_menor = num;
+                }  
+            }
 
-           
-            if (con == 1){
+            // Punto C
+            if (num > 0){
+                posi++;
+                if(posi == 1){
+                    ultimo_posi = num;
+                }else if(posi > 1){
+                    ante_posi = ultimo_posi;
+                    ultimo_posi = num;
+                }
+            }
+            
+            // Estructura
+            if (con == 1){ 
                 anterior = num;
             }else if(num < anterior){
                 banadera_mayor = false;
@@ -71,12 +97,25 @@ int main(){
         }else{
             cout << "No hay numeros primos en el grupo " << x << endl;
         }
-            
-            
-        
+        // Punto B
+        if(par > 0){
+            cout << "Grupo: " << x << ". Número par menor: " << par_menor << endl;
+        }else{
+            cout << "No hay números pares en el grupo: " << x << endl;
+        }
+        // Punto C   
+        if (posi == 1){
+            cout << "Último número positivo en el grupo " << x << " ---> " << ultimo_posi << endl;
+        }else if(posi > 1){
+            cout << "Grupo: "<< x << ". Anteúltimo número positivo: " << ante_posi << endl;
+            cout << "Grupo: "<< x << ". Último número positivo: " << ultimo_posi << endl;
+        }else{
+            cout << "Grupo " << x << ". No se ingresaron números positivos." << endl;
+        }
+       
     }
     
-
+    cout << endl << "--- FIN DEL PROGRAMA ---" << endl;
 
     return 0;
 }
