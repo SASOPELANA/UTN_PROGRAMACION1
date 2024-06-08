c02b19b9dc1d2dbf561ccce61c8fd402abb6149c
sergio <sopekof@gmail.com>
2024-06-08 17:59:01 -0300

    08/06/24

---
+++ Unidad6_Vectores/cpp/Ejercicio3.cpp
@@ -0,0 +1,12 @@
+/*
+    3 Leer 10 números y guardarlos en un vector. Determinar e informar cuál es el valor máximo y su posición dentro del vector.
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    
+
+    return 0;
+}
