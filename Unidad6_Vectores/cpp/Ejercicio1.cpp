ebf8594a3fe46e922fff8077511dcdea948194c8
sergio <sopekof@gmail.com>
2024-06-08 01:24:34 -0300

    Vectores

---
+++ Unidad6_Vectores/cpp/Ejercicio1.cpp
@@ -0,0 +1,28 @@
+/*
+    1 Hacer un programa para ingresar 10 números y guardarlos en un vector.
+    Determinar e informar cuál es la suma de los valores del vector
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    vector<int> Mi_vector(10);
+
+    int num, suma = 0, x = 0;
+    int acu = 0;
+
+    for (x = 0; x < 10; x++)
+    {
+        cout << "Ingrese un número: " << endl;
+        cin >> num;
+        suma += Mi_vector[x]= num; 
+        acu += num;
+    }
+    
+    cout << "La suma del Vector: " << suma << endl;
+    cout << "Suma de acumulador: " << acu << endl;
+
+    return 0;
+}
