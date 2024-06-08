83985af2aa0fd5cdbe8f71da30936c0f4c9f7fea
sergio <sopekof@gmail.com>
2024-06-08 19:14:01 -0300

    08

--- Unidad6_Vectores/cpp/Ejercicio3.cpp
+++ Unidad6_Vectores/cpp/Ejercicio3.cpp
@@ -6,7 +6,29 @@ using namespace std;
 
 int main(){
 
+    int vector[10];
+    int num, maximo = 0, posicion = 0, x = 0;
+
+    for (x = 0; x < 10; x++)
+    {
+        cout << "Ingrese números: " ;
+        cin >> num;
+        vector[x] = num;
+    }
     
+    maximo = vector[0];
+    posicion = 1;
+    for (x = 0; x < 10; x++)
+    {
+        if(vector[x] > maximo){
+            maximo = vector[x];
+            posicion = x + 1;
+        }
+    }
+    
+    cout << endl;
+    cout << "Máximo: " << maximo << endl;
+    cout << "Posición: " << posicion << endl;
 
     return 0;
 }
